c3c814b4bf732b612f53e29a8ea87285e98d3fc7
Mark Raitman <markraitman@mail.ru>
2023-03-31 17:16:37 +0300

    UIAlert function
    Create add item UIAlert function

--- Todoey/Controllers/TodoListTableViewController.swift
+++ Todoey/Controllers/TodoListTableViewController.swift
@@ -10,38 +10,82 @@ import UIKit
 class TodoListTableViewController: UITableViewController {
     
     //MARK: - Properties
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    //MARK: - Actions
+    // Эта функция вызывается всякий раз, когда пользователь нажимает кнопку "+" на панели навигации
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        // Создание пустого текстового поля для ввода
+        var textField = UITextField()
+        
+        // Создание всплывающего окна оповещения с заголовком "Добавить новый элемент Todoey"
+        let alert = UIAlertController(title: "Добавить новый элемент Todoey", message: "", preferredStyle: .alert)
+        
+        // Создание действия "Добавить элемент", которое будет выполняться при нажатии на соответствующую кнопку
+        // Данный блок кода добавляет введенный пользователем текст в массив itemArray и обновляет таблицу tableView
+        let action = UIAlertAction(title: "Добавить элемент", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        // Добавление текстового поля во всплывающее окно оповещения
+        // Блок кода задает место для ввода текста внутри всплывающего окна оповещения
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Создать новый элемент"
+            textField = alertTextField
+        }
+        
+        // Добавление действия во всплывающее окно оповещения
+        alert.addAction(action)
+        
+        // Отображение всплывающего окна оповещения на экране
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Возвращает количество элементов в массиве itemArray
         return itemArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Получение ячейки таблицы с идентификатором ToDoItemCell по индексу
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-
+    
+        // Задание текста для отображения в текущей ячейке таблицы
+        // Берется соответствующий элемент из массива itemArray с помощью индекса indexPath.row
         cell.textLabel?.text = itemArray[indexPath.row]
-
+    
+        // Возврат ячейки таблицы
         return cell
     }
     
+    
     //MARK: - Table view delegate methods
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-    }
+   // Эта функция вызывается при выборе пользователем строки в таблице.
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // Выводим элемент, соответствующий выбранной строке.
+       print(itemArray[indexPath.row])
+       
+       // Снимаем выделение с строки и делаем анимацию, если это нужно.
+       tableView.deselectRow(at: indexPath, animated: true)
+       
+       // Проверяем, есть ли уже галочка у выбранной ячейки.
+       if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+           // Если есть, удаляем галочку.
+           tableView.cellForRow(at: indexPath)?.accessoryType = .none
+       } else {
+           // Если нет, добавляем галочку к выбранной ячейке.
+           tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+       }
+   }
+   
 
     /*
     // Override to support conditional editing of the table view.
